545a35e284c8a15f35e75cfcd7a6e79cc5c1d2af
Jeff <jeffreysaa@outlook.com>
2025-09-30 15:36:52 -0500

    feat: test module

--- pos/__manifest__.py
+++ pos/__manifest__.py
@@ -1,35 +1,28 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "my_module",
+    'name': "Test Module",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Simple test module for Odoo.sh validation""",
 
     'description': """
-        Long description of module's purpose
+        A minimal test module to verify Odoo.sh is working correctly
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Insotech",
+    'website': "http://www.insotech.com",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Tools',
+    'version': '1.0',
 
-    # any module necessary for this one to work correctly
     'depends': ['base'],
 
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+
+    'installable': True,
+    'application': True,
+    'auto_install': False,
 }
--- pos/models/models.py
+++ pos/models/models.py
@@ -1,15 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields
 
-# class my_module(models.Model):
-#     _name = 'my_module.my_module'
+class TestRecord(models.Model):
+    _name = 'test.record'
+    _description = 'Test Record'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    name = fields.Char(string='Name', required=True)
+    description = fields.Text(string='Description')
